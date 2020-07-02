8790bbaf869da5e8154058e5301489aa6d7d17da
NirAr <NirAr@NIRARM-LAP>
2020-07-02 09:46:48 +0300

    readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # Delay Service
 
 [![N|Solid](https://cldup.com/dTxpPi9lDf.thumb.png)](https://nodesource.com/products/nsolid)
@@ -9,7 +10,7 @@ Don't worry, even if the service is down it will still print your messages when 
 
 #### tl;dr
 
-For this project I used [Express] and [Redis] sorted sets, queues, pub/sub, locks and event listeners
+For this project I used [ExpressJS](https://expressjs.com) and [Redis](https://redis.io) sorted sets, lists, pub/sub, locks and event listeners.
 I used two approaches for delay queue management:
  - Pulling - using Redis as worker queue to pull messages that should be sent now
  - Event base -  using Redis's keyspace event notification in this case expiration events
@@ -34,6 +35,53 @@ If there is a message(s) to send the service will push it into a queue, publish 
 Using a list should ensure us that only one subscriber will pop the message from the top of the list, it also locks it by uuid to ensure that only one subscriber will print it. 
 
 The service is both the publisher and the subscriber but I also added a subscriber.js.
+
 [This is suggested on Redis's e-book](https://redislabs.com/ebook/part-2-core-concepts/chapter-6-application-components-in-redis/6-4-task-queues/6-4-2-delayed-tasks/)
 
 #### Event Driven
+Pulling is great but it also very heavy on the system as we keep querying Redis over and over.
+Usually for those kind of offline tasks an event driven approach is more effective.
+For this I used Redis's *[keyspace event notification](https://redis.io/topics/notifications)* in this case expiration events.
+
+When a message is received from the API the service will add it to the sorted set but will also add a scheduled task for expiration event, when receiving the event the subscriber will print the message and will delete it from the set.
+To ensure only one subscriber prints the message I used a lock on the message UUID. this is optimistic concurrency approach - only one will succeed to lock the message and other instances will fail
+
+
+### Installation
+
+For events driven the keyspace event notification should be set as follow on Redis startup (or added to redis.config)
+please note: keyspace event notification has some overhead on Redis so it is disabled as default
+
+    ./redis-server --notify-keyspace-events Ex
+
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+npm install -d
+node app
+```
+Set the configuration environment
+
+     export NODE_ENV=local
+
+#### Windows:
+
+    setx NODE_ENV local
+### Documentation
+you can see the Service's API calls here:
+[http://localhost:3000/doc](http://localhost:3000/doc)
+
+### Known Issues (AKA things I should've done better)
+
+ - The services is based on "Spaghetti Code" design pattern, i should've module the folders and files better
+ - Redis connections- I might have handle Redis connection better, I used a single function to connect Redis each time it was needed, but I'm not sure I closed all the connections properly 
+ - **TESTING!** 
+### Blogs and Repositories 
+ - [Redis keyspace event notification](http://blog.codezuki.com/blog/2013/07/07/redis-queue)
+ - [Config file](https://codeburst.io/config-module-cleaner-way-to-write-nodejs-configuration-files-cd96ecffbde7)
+ - [Delayed Tasks](https://redislabs.com/ebook/part-2-core-concepts/chapter-6-application-components-in-redis/6-4-task-queues/6-4-2-delayed-tasks/)
+ - [RedisInsight](https://redislabs.com/redisinsight/)
+ - [Publish/subscribe](https://redislabs.com/ebook/part-2-core-concepts/chapter-3-commands-in-redis/3-6-publishsubscribe/)
+ - [Sorted Sets](https://redislabs.com/ebook/part-2-core-concepts/chapter-3-commands-in-redis/3-5-sorted-sets/)
+ - [Lists](https://redislabs.com/ebook/part-2-core-concepts/chapter-3-commands-in-redis/3-2-lists/)
--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const redis = require("redis");
 const Redlock = require('redlock');
 const Scheduler = require('redis-scheduler');
 const _key = 'messages';
+const _notificationQueue = 'notification'
+const _messageToSendQueue = 'messages_to_send'
+const _checkLaterQueue = 'check_later_queue'
 const config = require('config');
 const _redisPort = config.get('redis.port');
 const _redisHost = config.get('redis.host');
@@ -81,20 +84,20 @@ var jsonParser = bodyParser.json()
  *         description: Message time stamp is older than the current the time
  */
 app.post('/api/v1/echoAtTime', jsonParser, function (req, res) {
-    var d = new Date(req.body.date).getTime();
-    if (d < Date.now()) {
+    var requestedDate = new Date(req.body.date).getTime();
+    // validate that the given date is in the future - if not return 400
+    if (requestedDate < Date.now()) {
         res.status(400).send();
     } else {
-
         redisConnection((client, locker, scheduler) => {
             var uuid = uuidv1();
-            var json = JSON.stringify({ message: req.body.message, uuid: uuid, time: d });
-            client.zadd(_key, d, json);
+            var json = JSON.stringify({ message: req.body.message, uuid: uuid, time: requestedDate });
+            client.zadd(_key, requestedDate, json);
             //add expersion event 
-            scheduler.schedule({ key: json, expire: d - Date.now(), handler: eventTriggered }, function (err) {
+            scheduler.schedule({ key: json, expire: requestedDate - Date.now(), handler: eventTriggered }, function (err) {
             });
             client.quit();
-            res.status(201).send('Event was added for, ' + d)
+            res.status(201).send('Event was added for, ' + requestedDate)
         });
     }
 });
@@ -133,16 +136,17 @@ app.post('/api/v1/echoAtTime', jsonParser, function (req, res) {
  *         description: Message time stamp is older than the current the time
  */
 app.post('/api/v2/echoAtTime', jsonParser, function (req, res) {
-    var d = new Date(req.body.date).getTime();
-    if (d < Date.now()) {
+    var requestedDate = new Date(req.body.date).getTime();
+    // validate that the given date is in the future - if not return 400
+    if (requestedDate < Date.now()) {
         res.status(400).send
     } else {
         redisConnection((client, locker, scheduler) => {
             var uuid = uuidv1();
-            var json = JSON.stringify({ message: req.body.message, uuid: uuid, time: d });
-            client.zadd(_key, d, json);
+            var json = JSON.stringify({ message: req.body.message, uuid: uuid, time: requestedDate });
+            client.zadd(_key, requestedDate, json);
             client.quit();
-            res.status(201).send('Event was added for, ' + d)
+            res.status(201).send('Event was added for, ' + requestedDate)
         });
 
         res.status(200).send();
@@ -165,14 +169,14 @@ function eventTriggered(err, key) {
     });
 }
 
-app.listen(port, function  () {
+app.listen(port, function () {
     var now = Date.now();
 
     //"check later" subscriber will keep checking if there is a message with the current timestamp (+500 milli)
     // if there are messages to print it will try to lock them, remove them from the ZLIST, add them tho the worker queue and publish an event
     // for the worker to print the message
     var CheckLaterSubscriber = redis.createClient(_redisPort, _redisHost);
-    CheckLaterSubscriber.subscribe('check_later');
+    CheckLaterSubscriber.subscribe(_checkLaterQueue);
 
     CheckLaterSubscriber.on('message', function (channel, message) {
         redisConnection((client, locker) => {
@@ -183,8 +187,8 @@ app.listen(port, function  () {
                     // lock the message by uuid so no other service will be able to use it
                     // if the lock can't be acquired it means that other instance already got it and will send it to the worker queue
                     locker.lock('lock:' + res.uuid, 1000).then(function (lock) {
-                        client.lpush('messages_to_send', element[0]);
-                        publishMessage('notification', 1);
+                        client.lpush(_messageToSendQueue, element[0]);
+                        publishMessage(_notificationQueue, 1);
                         client.zrem(_key, element[0]);
                         return lock.unlock()
                             .catch(function (err) {
@@ -194,19 +198,19 @@ app.listen(port, function  () {
             });
 
         })
-        // in any case - repopulate the quere for next job
-        publishMessage('check_later', 1);
+        // in any case - repopulate the queue for next iteration
+        publishMessage(_checkLaterQueue, 1);
 
     });
 
     //notifcation subscriber will get notification to pop a message from the "messages_to_send" queue , will try to lock it and print the message to the console
     var notificationsSubscriber = redis.createClient(_redisPort, _redisHost);
-    notificationsSubscriber.subscribe('notification');
+    notificationsSubscriber.subscribe(_notificationQueue);
 
     notificationsSubscriber.on('message', function (channel, message) {
         redisConnection((client, locker, scheduler) => {
             //poping the first message in the queue
-            client.lpop(['messages_to_send'], function (err, reply) {
+            client.lpop([_messageToSendQueue], function (err, reply) {
                 if (reply != null) {
                     var res = JSON.parse(reply);
                     //try to accuire lock
@@ -221,14 +225,14 @@ app.listen(port, function  () {
         });
     });
 
-    // init the first time 
-    publishMessage('check_later', 1);
+    // init the first iteration 
+    publishMessage(_checkLaterQueue, 1);
 
-    // check if there are messages in the queue that were not printed on time becausr the server was down
+    // check if there are messages in the queue that were not printed on time because the server was down
     redisConnection((client, locker) => {
         // get all messages that are still availble in the queue that should've been sent by now
         // when all goes well this query should return 0 results as 
-        // 1. there are messages in the queue but they should be sent in later time
+        // 1. there are messages in the queue but they will be sent in later time
         // 2. other instances already proceesed the messages on time
         client.zrangebyscore(_key, -1, now, 'withscores', function (err, members) {
             var chunck = _.chunk(members, 2);
@@ -254,9 +258,9 @@ function publishMessage(queueName, msg) {
     var publisher = redis.createClient(_redisPort, _redisHost);
     publisher.publish(queueName, msg);
     publisher.quit();
-
 }
 
+//helper function for redis connection 
 function redisConnection(redisFunction) {
     var client = redis.createClient(_redisPort, _redisHost);
     var redlock = new Redlock([client]);
